64e62518d11bb9f56a96c30c6aba9ddd4f6458ad
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-30 10:04:08 +0500

    before & after

--- Node.js-Unit-Testing-In-Depth/test/test.js
+++ Node.js-Unit-Testing-In-Depth/test/test.js
@@ -2,16 +2,31 @@ const assert = require('assert');
 
 describe('file to be tested' , () => {
     context('function to be tested' , () => {
+        before(() => {
+            console.log('Before test!');
+        });
+
+        after(() => {
+            console.log('After test!');
+        });
+
+        beforeEach(() => {
+            console.log('--beforeEach');
+        });
+
+        
+        afterEach(() => {
+            console.log('--afterEach');
+        });
+
         it('should do something' , () => {
             assert.equal(1,1);
         });
 
         it('should do something else' , () => {
-            assert.deepEqual({name:'John'} , {name:'sam'});
+            assert.deepEqual({name:'John'} , {name:'John'});
         });
 
-        it('this is a pending test' , () => {
-
-        });
+        it('this is a pending test');
     });
 });
